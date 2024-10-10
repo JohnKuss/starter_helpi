b934c1f5c68f60025c9f52f667ccecb088864a6c
John Kuss <143646569+JohnKuss@users.noreply.github.com>
2024-10-10 17:07:06 -0400

    Added a concise description for the basic career assessment.

--- src/BasicAssessment.tsx
+++ src/BasicAssessment.tsx
@@ -2,6 +2,7 @@ export function BasicCareerAssessment(): React.JSX.Element {
     return (
         <div className="basicAssessment">
             <header>Basic Career Assessment</header>
+            <div>Basic Career Assessment: Provides a series of more basic questions and generates results for potential careers.</div>
         </div>
     )
 } 
