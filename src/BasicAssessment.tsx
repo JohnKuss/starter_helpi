a507a5aece3c16c784230e7f36f6597195112722
robertkuss <robertjk@udel.edu>
2024-10-11 12:59:15 -0400

    Added estimated time to BasicAssessment.tsx

--- src/BasicAssessment.tsx
+++ src/BasicAssessment.tsx
@@ -2,6 +2,7 @@ export function BasicCareerAssessment(): React.JSX.Element {
     return (
         <div className="basicAssessment">
             <header>Basic Career Assessment</header>
+            <p>Estimated time: 5-10 minutes</p>
         </div>
     )
 } 
