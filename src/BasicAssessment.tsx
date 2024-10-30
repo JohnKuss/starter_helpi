15628c12c7f0b2a2b5380c0fadb3e38405f045cd
John Kuss <143646569+JohnKuss@users.noreply.github.com>
2024-10-30 13:22:46 -0400

    Re-formatted all questions to be in-line with wireframe for basic career assessment. Still need to implement each questions respective answers.

--- src/BasicAssessment.tsx
+++ src/BasicAssessment.tsx
@@ -12,12 +12,7 @@ export function BasicCareerAssessment(): React.JSX.Element {
   const incrementAnsweredQuestions = () => {
     setAnsweredQuestions(prev => prev + 1);
   };
-  //Array of questions.
-  const basicQuestions = ["I prefer a structured work enviroment.","I am fine traveling for work.",
-    "I like working with others.","I am a good leader.","I prefer a creative job.",
-    "I would prefer to on-site.","I deal well with meetings and other formalities."];
-  //Array of answers.
-  const basicAnswers = ["Very not like me","Not like me","Somewhat like me","Like me","Very like me"];
+  
   return (
     <div className="basicAssessment">
       <h1>Basic Career Assessment</h1>
@@ -34,17 +29,44 @@ export function BasicCareerAssessment(): React.JSX.Element {
       <p>Progress: {answeredQuestions}/{totalQuestions} questions answered</p>
 
       {/* You can add questions later and call incrementAnsweredQuestions when a question is answered */}
-        {basicQuestions.map((question:string) => <div>
-          {question+" "}
-          {basicAnswers.map((answer:string) => 
-            <Form.Check 
-            inline
-            type="radio"
-            name={question}
-            label={answer}
-            />
-            )}
-          </div> )}
+      <Form.Group controlId="idealEnvironment">
+        <Form.Label>1. Ideal work enviroment?</Form.Label>
+        {["Office","Remote","Hybrid","Outdoor","Other"].map((answer: string) => <Form.Check
+        inline
+        type="radio"
+        name="question1Answer"
+        key={answer}
+        label={answer}
+        value={answer}
+        />)}
+      </Form.Group>
+      <Form.Group controlId="preferredWorkday">
+        <Form.Label>2. Preferred workday?</Form.Label>
+        {["Solo Tasks","Team projects","Client interactions","Leading projects","Other"].map((answer:string) => <Form.Check
+        inline
+        type="radio"
+        name="question1Answer"
+        key={answer}
+        label={answer}
+        value={answer}
+        />)}
+      </Form.Group>
+      <Form.Group controlId="stabilityImportance">
+        <Form.Label>3. Job stability importance?</Form.Label>
+        <></>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>4. Problem-Solving Style?</Form.Label>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>5. Biggest motivator?</Form.Label>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>6. How do you handle pressure?</Form.Label>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>7. How do you feel about travel for work?</Form.Label>
+      </Form.Group>
       {/* Not functional yet */}
       <Button variant="secondary">Pause Button</Button>
     </div>
