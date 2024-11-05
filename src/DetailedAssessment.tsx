d8b790b86c515fb901b9a0b45fd859129bd7223d
Lena <lenamoha@udel.edu>
2024-11-04 23:32:48 -0500

    Fixed issues + added questions numbers

--- src/DetailedAssessment.tsx
+++
@@ -1,7 +0,0 @@
-export function DetailedAssessment(): React.JSX.Element {
-    return (
-        <div className="DetailedAssessment">
-            <header>Detailed Career Assessment</header>
-        </div>
-    )
-} 
